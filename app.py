66bc2924b454ab2be674f5bed56d6810011a93b7
conno <22816@stpauls.qld.edu.au>
2023-04-21 19:40:02 +1000

    21-04-23_01

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import json
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key :)'
+
+
+url = "https://theaudiodb.p.rapidapi.com/523532/searchalbum.php"
+
+
+@app.route('/')
+@app.route('/index', methods=['GET','POST'])
+def index():
+    # req = requests.get(url)
+    # source = req.text
+    # data = json.loads(source)
+    # print(type(source))
+    # print(type(data))
+    # print(data)
+    # print(data.keys())
+    # print(type(data['track']))
+    # # print(type(data['track'].keys()))
+    # print((data['track']))
+    # mydata = data['track']
+    # return render_template('index.html', data=mydata)
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
