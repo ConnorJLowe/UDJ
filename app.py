e42f17ab485e16f491a87d9eb23e9c194f7da692
conno <22816@stpauls.qld.edu.au>
2023-04-21 20:18:18 +1000

    21-04-23_01

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ app.config['SECRET_KEY'] = 'your secret key :)'
 
 url = "https://theaudiodb.p.rapidapi.com/523532/searchalbum.php"
 
+# creating a variable to set up methods as GET and POST
+methods = ['GET', 'POST']
+
 
 @app.route('/')
-@app.route('/index', methods=['GET','POST'])
+@app.route('/index', methods=methods)
 def index():
     # req = requests.get(url)
     # source = req.text
@@ -26,7 +29,6 @@ def index():
     # mydata = data['track']
     # return render_template('index.html', data=mydata)
     return render_template('index.html')
-
 
 
 if __name__ == '__main__':
